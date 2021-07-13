64a53396dc30a470f65107dc7d58b29e272d2faf
Ankit Goyal <layog@google.com>
2021-07-13 17:21:51 +0800

    mali_kbase: platform: Add per-process and global sysfs nodes for GPU mem usage
    Bug: 191966412
    Signed-off-by: Ankit Goyal <layog@google.com>
    Change-Id: Id47feadaf9da7ef8e22494ab64e6263d7f87213c
    Signed-off-by: nishant6342 <www.rajsonu13@gmail.com>

--- drivers/gpu/mediatek/gpu_mali/mali_bifrost/mali-r25p0/drivers/gpu/arm/midgard/context/mali_kbase_context.c
+++ drivers/gpu/mediatek/gpu_mali/mali_bifrost/mali-r25p0/drivers/gpu/arm/midgard/context/mali_kbase_context.c
@@ -145,6 +145,10 @@ static int kbase_insert_kctx_to_process(struct kbase_context *kctx)
 		kprcs->dma_buf_root = RB_ROOT;
 		kprcs->total_gpu_pages = 0;
 		kprcs->dma_buf_pages = 0;
+		WARN_ON(kobject_init_and_add(
+					&kprcs->kobj, &kprcs_ktype,
+					kctx->kbdev->proc_sysfs_node,
+					"%d", tgid));
 
 		while (*new) {
 			struct kbase_process *prcs_node;
