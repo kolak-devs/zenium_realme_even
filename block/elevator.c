61d55017fd0bf6290f1a8ec5e18a55a0144caf7d
Juhyung Park <qkrwngud825@gmail.com>
2022-06-17 17:45:57 +0900

    block: use ssg by default
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>
    [adithya2306: depend on the config]
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: wulan17 <wulan17@komodos.id>

--- block/elevator.c
+++ block/elevator.c
@@ -1015,7 +1015,13 @@ int elevator_init_mq(struct request_queue *q)
 	if (unlikely(q->elevator))
 		goto out;
 
-	e = elevator_get(q, "mq-deadline", false);
+	e = elevator_get(q,
+#ifdef CONFIG_MQ_IOSCHED_SSG
+	"ssg",
+#else
+	"mq-deadline",
+#endif
+	false);
 	if (!e)
 		goto out;
 
